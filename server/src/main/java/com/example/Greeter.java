bf7c35df400693df3c65352869ab075181fefc84
Ankit Sheth <ankits21@kpit.com>
2018-08-18 14:02:34 +0530

    for error resolving

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
  /**
-  * @param : some name of person
-  * @return : name of the string
+  * @param : someone
+  * @return : string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
